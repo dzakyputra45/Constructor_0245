935db8216275ae475a767ebd5823962c8cfda8b4
dzakyputra45 <dzakypratama45@gmail.com>
2024-05-14 16:00:33 +0700

    Menyelesaikan Destruktor

--- destruktor.cpp
+++ destruktor.cpp
@@ -3,11 +3,46 @@ using namespace std;
 
 class angka{
 private:
-    int*arr;
+    int *arr;
     int panjang;
 public:
-    angka(int); //constructor
-    ~angka();   //destructor
+    angka(int);     //construktor
+    ~angka();       //destructor
     void cetakData();
     void isiData();
 };
+
+angka::angka(int i) {
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
+
+angka::~angka(){    //destruktor
+    cout << endl;
+    cetakData();
+    delete[]arr;
+    cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
+
+void angka::cetakData(){
+    for(int i=1;i<=panjang;i++){
+        cout << i << "="<<arr[i]<<endl;
+    }    
+}
+
+void angka::isiData(){
+    for(int i=1;i<=panjang;i++){
+        cout <<i<<" = ";
+        cin>>arr[i];
+    }
+    cout<<endl;
+}
+
+int main(){
+    angka belajarcpp(3);                    //Construktor Dipanggil
+    angka *ptrBelajarcpp = new angka(5);    //Construktor Dipanggil
+    delete ptrBelajarcpp;                   //Destructor Dipanggil
+
+    return 0;
+}//Destruktor Dipanggil
